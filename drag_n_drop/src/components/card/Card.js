d5bd378cc186b2139d7993de3bcb0bfdd0f9eb63
Vinay Prasad N <vinaypn16@gmail.com>
2022-12-20 17:11:18 +0530

    listener moved to new function

--- drag_n_drop/src/components/card/Card.js
+++ drag_n_drop/src/components/card/Card.js
@@ -4,20 +4,24 @@ import styles from "./Card.module.css";
 const Card = ({ cardData = {} }) => {
 	const [cardIsBeingDragged, setCardIsBeingDragged] = useState(false);
 
+	const handleOnDragStart = (ev) => {
+		let draggedItemData = ev.target.getAttribute("data_card_json") || "{}";
+		ev.dataTransfer.setData("draggedItemData", draggedItemData);
+		setCardIsBeingDragged(true);
+	}
+
+	const handleOnDragEnd = () => {
+		setCardIsBeingDragged(false);
+		// console.log("drag stopped");
+	}
+
 	return (
 		<div
 			className={`${styles.card} ${cardIsBeingDragged ? styles.reducedOpacity : ""}`}
 			data_card_json={JSON.stringify(cardData)}
 			draggable
-			onDragStart={(ev) => {
-				let draggedItemData = ev.target.getAttribute("data_card_json") || "{}";
-				ev.dataTransfer.setData("draggedItemData", draggedItemData);
-				setCardIsBeingDragged(true);
-			}}
-			onDragEnd={() => {
-				setCardIsBeingDragged(false);
-				// console.log("drag stopped");
-			}}
+			onDragStart={handleOnDragStart}
+			onDragEnd={handleOnDragEnd}
 		>
 			<h3>{cardData.name}</h3>
 			<h3>State - {cardData.state}</h3>
