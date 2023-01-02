9dd2c02ccb6d883db4108ce79208641a31b6841a
Vinay Prasad N <vinaypn16@gmail.com>
2023-01-02 20:29:43 +0530

    tailwind setup

---
+++ ui_libraries/tailwind/README.md
@@ -0,0 +1,43 @@
+## Tailwind Setup
+
+-   Install the following.\
+    `npm install -D tailwindcss autoprefixer postcss-cli mini-css-extract-plugin postcss-loader postcss postcss-preset-env`
+
+-   Initialize the tailwind config file.\
+    `npx tailwindcss init`
+
+-   In the tailwind config file, setup the path for all the source files.
+
+    ```
+    module.exports = {
+    	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    	theme: {
+    		extend: {},
+    	},
+    	plugins: [],
+    };
+
+    ```
+
+-   Create and setup the `postcss.config.js`
+
+    ```
+    const tailwindcss = require("tailwindcss");
+    module.exports = {
+    	plugins: ["postcss-preset-env", tailwindcss],
+    };
+    ```
+
+-   Add `postcss-loader` under css rules in webpack.
+
+-   Create `index.css` and add the following in it,
+
+    ```
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+    ```
+
+-   Import the `index.css` in the `index.js` file.
+
+---
---
+++ ui_libraries/tailwind/webpack/webpack.common.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+	entry: path.resolve(__dirname, "..", "./src/index.js"),
+	output: {
+		path: path.resolve(__dirname, "..", "./build"),
+		filename: "bundle.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "..", "./src/index.html"),
+		}),
+		new Dotenv(),
+	],
+	resolve: {
+		extensions: [".jsx", ".js"], //	This allows us to leave the file extensions when importing. Webpack checks the files with extensions in the given order.
+		alias: {
+			Root: path.resolve(__dirname, "..", "./"),
+			Src: path.resolve(__dirname, "..", "./src/"),
+			Asset: path.resolve(__dirname, "..", "./src/assets/"),
+			Component: path.resolve(__dirname, "..", "./src/components/"),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)x?$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+							modules: {
+								auto: true,
+								localIdentName: "[hash:base64]",
+							},
+						},
+					},
+					"postcss-loader",
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+							modules: {
+								auto: true,
+								localIdentName: "[hash:base64]",
+							},
+						},
+					},
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: "asset/inline",
+			},
+		],
+	},
+};
