4d1cc18d95fe326fc1dee7daa246b3a26d5668c1
Vinay Prasad N <vinaypn16@gmail.com>
2023-01-05 16:20:46 +0530

    hoc added

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Index
 
-1. [Styled components](./ui_libraries/styled_components/README.md)
+1. [Styled Components](./ui_libraries/styled_components/README.md)
+1. [Higher Order Components](./miscellaneous_concepts/hoc/README.md)
 
 ---
 
---
+++ miscellaneous_concepts/hoc/src/App.js
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+
+const App = () => {
+	return (
+		<div className={styles.app}>
+			<h2 className={styles.heading}>App</h2>
+			<p className={styles.title}>This is a title</p>
+		</div>
+	);
+};
+
+export default App;
