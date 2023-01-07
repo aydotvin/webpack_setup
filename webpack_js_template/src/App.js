d481ab3fb0c918f454bb2492a0dfd55ba42fbaa8
Vinay Prasad N <vinaypn16@gmail.com>
2023-01-07 14:13:46 +0530

    js template styles organized

--- webpack_js_template/src/App.js
+++ webpack_js_template/src/App.js
@@ -2,16 +2,14 @@ import styles from "./App.module.scss";
 import Counter from "Component/counter/Counter";
 
 const App = () => {
-    console.log(process.env.ENVKEY1);
-    console.log(process.env.flag1);
+	console.log(process.env.ENVKEY1);
+	console.log(process.env.flag1);
 
-    return (
-        <div className={styles.app}>
-            <h2 className={styles.heading}>App</h2>
-            <p className={styles.title}>This is a title</p>
-            <Counter></Counter>
-        </div>
-    );
+	return (
+		<div className={`flex-col ${styles.app}`}>
+			<Counter></Counter>
+		</div>
+	);
 };
 
 export default App;
--- webpack_js_template/src/index.js
+++ webpack_js_template/src/index.js
@@ -1,4 +1,5 @@
 import ReactDom from "react-dom/client";
+import "Style/index.scss";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
