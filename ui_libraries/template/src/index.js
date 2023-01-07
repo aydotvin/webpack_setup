50cbcaf4a553e168838b04b502782d4cb053db6b
Vinay Prasad N <vinaypn16@gmail.com>
2023-01-07 14:53:33 +0530

    clean up of ui template

--- ui_libraries/template/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDom from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App></App>);
